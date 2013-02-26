fe0291a02910b72293452ce6f2ffe8e150fec173
Michael 'myrhev' Mathieu <mathieu@cs.nyu.edu>
2013-02-25 21:18:48 -0500

    Options

--- learn.lua
+++ learn.lua
@@ -2,21 +2,45 @@ require 'butterfly'
 require 'nn'
 require 'optim'
 require 'randomRotation'
+require 'xlua'
 
-local n = 6
+op = xlua.OptionParser('%prog [options]')
+op:option{'-n', '--n', action='store', dest='n',
+	  default = 6, help='n=lg(N) where N is the size of the matrix'}
+op:option{'-e', '--num-epochs', action='store', dest='nEpochs',
+	  default=1000, help='Number of epochs'}
+op:option{'-s', '--num-samples', action='store', dest='nSamples',
+	  default=500, help='Size of the training set'}
+op:option{'-norm', '--k-normalize', action='store', dest='kNormalize',
+	  default=10, help='Number of SGD iterations between two normalizations'}
+op:option{'-b', '--butterfly-target', action='store_true', dest='butterfly_target',
+	  default=false, help='The target rotation matrix is a butterfly rotation'}
+op:option{'-lr', '--learning-rate', action='store', dest='learning_rate',
+	  default = 1, help='Learning rate'}
+op:option{'-lrd', '--learning-rate-decay', action='store',
+	  dest='learning_rate_decay', default = 1e-3, help='Learning rate decay'}
+opt = op:parse()
+
+local n = tonumber(opt.n)
 local N = 2^n
---local butterfly = torch.randn(n,N/2)
---local targetMat = bf2mat(butterfly)
-local targetMat = randomRotation(N)
+local targetMat
+if opt.butterfly_target then
+   local butterfly = torch.randn(n,N/2)
+   targetMat = bf2mat(butterfly)
+else
+   targetMat = randomRotation(N)
+end
 
 local butterfly2 = torch.randn(n,N/2)
 local net = bf2nn(butterfly2)
 
-local nEpochs = 10000
-local nSamples = 100
+local nEpochs = tonumber(opt.nEpochs)
+local nSamples = tonumber(opt.nSamples)
+local kNormalize = tonumber(opt.kNormalize)
 local criterion = nn.MSECriterion()
-local config = {learningRate = 1, weightDecay = 0,
-		momentum = 0, learningRageDecay = 1e-4}
+local config = {learningRate = tonumber(opt.learning_rate), weightDecay = 0,
+		momentum = 0, learningRageDecay = tonumber(opt.learning_rate_decay)}
+print(config)
 local parameters, gradParameters = net:getParameters()
 local samples = torch.randn(nSamples, N)
 
@@ -38,8 +62,21 @@ for iEpoch = 1,nEpochs do
 		       return err, gradParameters
 		    end
       optim.sgd(feval, parameters, config)
-      bfNormalize(net)
+      if iSample % kNormalize == 0 then
+	 bfNormalize(net)
+      end
    end
+   
+   --[[
+   input = samples[1]
+   target = torch.mm(targetMat, input:reshape(input:size(1),1))
+   local output = net:forward(input)
+   local toprint = torch.Tensor(output:size(1), 2)
+   toprint[{{},1}]:copy(output)
+   toprint[{{},2}]:copy(target)
+   print(toprint)
+   --]]
+   
    meanErr = meanErr/nSamples
    print("meanErr="..meanErr.." (epoch "..iEpoch..")")
 end
